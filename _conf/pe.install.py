7e8dc471e6709c5d12603f7bfa25b72f4f418b14
JetLogic <biojetx@gmail.com>
2025-05-20 01:47:29 +0800

    use bin.tar.gz

--- _conf/pe.install.py
+++
@@ -1,6 +0,0 @@
-if profile.test("os.shell", "cmd"):
-    if profile.test("os.name", "posix"):
-        Directory("bin").call(target=Resource("../bin/mkudfimg.cmd"))
-elif profile.test("os.shell", "sh"):
-    if profile.test("os.name", "posix"):
-        Directory("bin").execute(target=Resource("../bin/mkudfimg.sh"), name="mkudfimg")
--- doc/doc.py
+++
@@ -1,5 +0,0 @@
-from argparse import ArgumentParser
-
-argp = ArgumentParser()
-
-# argp.add_argument("--output", "-o", )
